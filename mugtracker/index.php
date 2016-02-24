2cfa9386691fe1c4518af04a808f34d3f6f6e70d
0917852 <alapati.rgu@outlook.com>
2016-02-24 15:59:34 +0000

    my new project

--- mugtracker/index.php
+++ mugtracker/index.php
@@ -8,9 +8,11 @@ $myage = 56;
 echo "my name is" . $myname . "and i am" .$myage;
 
 ?>
+</p>
+<p>
 <?php
 $name = "yogitha";
-if($name == "yogitha")
+if($name == "pujitha")
 {
     print "i know you";
 }
@@ -19,5 +21,22 @@ else {
 }
 ?>
 </p>
+<p>
+    <?php
+    $myage = 25;
+    switch ($myage)
+    {
+        case 1:
+            echo "•	buy specs if you are over 16";
+        case 2:
+            echo "•	buy mugs if you are over 18";
+        case 3:
+            echo "buy sausage rolls if you are over 21";
+        default:
+            echo "if you are under 16 you are not allowed to buy";
+    }
+    ?>
+</p>
+
 </body>
 </html>
