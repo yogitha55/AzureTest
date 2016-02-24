5c4bfbd702781f4e7d5c903a6868ad0a60e0fce6
0917852 <alapati.rgu@outlook.com>
2016-02-24 15:33:04 +0000

    my new project

--- mugtracker/index.php
+++ mugtracker/index.php
@@ -20,6 +20,16 @@ $myage = 56;
 echo "my name is" . $myname . "and i am" .$myage;
 
 ?>
+<?php
+$name = "yogitha";
+if($name == "yogitha")
+{
+    print "i know you";
+}
+else {
+    print "who are you";
+}
+?>
     </p>
 </body>
 </html>
