280de696244ed17de89ecee472136f3d5941d0c7
0917852 <alapati.rgu@outlook.com>
2016-02-24 14:59:09 +0000

    my new project

---
+++ mugtracker/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0917852
+ * Date: 24/02/2016
+ * Time: 14:34
+ */
+?>
+<html>
+<head>
+</head>
+<body>
+<p>
+
+<?php
+echo "hello";
+?>
+    </p>
+</body>
+</html>
