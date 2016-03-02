43327a370745ec75bb77057d52b02be878b8ec49
0917852 <alapati.rgu@outlook.com>
2016-03-02 16:18:29 +0000

    my new project

---
+++ week 5/home.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello</h1>
+</body>
+</html>
+
---
+++ week 5/login.php
@@ -0,0 +1,23 @@
+<?php
+include("connection.php");
+if(empty($_POST["username"]) || empty($_POST["password"]))
+{
+    $echo = "Both fields are required.";
+}else
+{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
+
+    $result=mysqli_query($db,$sql);
+    if(mysqli_num_rows($result) == 1)
+    {
+        header("location: home.php"); // Redirecting To another Page
+    }else
+    {
+        $echo "Incorrect username or password.";
+        }
+
+}
+?>
+
