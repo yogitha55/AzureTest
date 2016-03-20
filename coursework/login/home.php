f3a1a5b9f6b218eab4d4d2a76ad97a2e13c88ad4
gith2 <hanuman55>
2016-03-20 12:46:39 +0000

    gg

--- coursework/login/home.php
+++ coursework/login/home.php
@@ -16,7 +16,7 @@
 <tr>
 	<td colspan="2">Bugs</td>
 	<td> <a href="http://yogitha.azurewebsites.net/coursework/bugs/bugs.php">Bugs Home</a> </td>
-</tr>
+</tr><br>
 <tr>
 	<td>
 		<a href="logout.php" style="font-size:18px">Logout?</a>
