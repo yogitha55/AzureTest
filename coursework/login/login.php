f808a31d2b6aa1732e5a9114d42f980cc78341e4
gith2 <hanuman55>
2016-03-20 11:51:04 +0000

    gg

--- coursework/login/login.php
+++ coursework/login/login.php
@@ -39,7 +39,7 @@
 				//If username and password exist in our database then create a session.
 			//Otherwise echo error.
 			
-			if(mysqli_num_rows($result) == 0)
+			if(mysqli_num_rows($result) == 1)
 			{
 				$_SESSION['username'] = $username; // Initializing Session
 				header("location: home.php"); // Redirecting To Other Page
