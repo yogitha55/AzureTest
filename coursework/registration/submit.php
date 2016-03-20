04c834120c8637680146b9ce65a8e404c10d5a4f
gith2 <hanuman55>
2016-03-20 00:03:15 +0000

    gg

--- coursework/registration/submit.php
+++ coursework/registration/submit.php
@@ -29,7 +29,7 @@ if(isset($_POST["submit"]))
     else
     {
        // echo $name." ".$email." ".$password;
-        $query = mysqli_query($db, "INSERT INTO users (user_userID, user_username, user_email, user_phone, user_password)VALUES ('$userId','$name', '$email', '$phone' '$password')")or die(mysqli_error($db));
+        $query = mysqli_query($db, "INSERT INTO users (userID, username, email, phone, password)VALUES ('$userId','$name', '$email', '$phone', '$password')")or die(mysqli_error($db));
         if($query)
         {
             $msg = "Thank You! you are now registered.";
