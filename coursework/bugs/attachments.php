b904450b4e11719e5614ebd86f67d488eeb83059
0917852 <alapati.rgu@outlook.com>
2016-03-21 12:11:01 +0000

    new message

--- coursework/bugs/attachments.php
+++ coursework/bugs/attachments.php
@@ -4,15 +4,43 @@ include("connection.php");
 include("submit.php");
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
+<head>
+    <meta charset="utf-8">
+    <title>Attachments</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
 <body>
+<form method="post" action="">
+    <fieldset>
+        <legend>Attachments</legend>
+        <table width="400"  border="0" cellpadding="10" cellspacing="10">
+            <tr>
+                <td colspan="2" align="centre" class="error"><?php echo $msg;?></td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold">
+                    <div align="right"><label for="name">File Upload</label></div>
+                </td>
+                <td>
+                    <input name="fileToUpload" type="file" id="fileToUpload"class="input">
+                    <input type="submit" value="Upload File" name="submit">
+                </td>
+            </tr>
+            <tr>
+                <td height="23"></td>
+                <td>
+                    <div align="right">
+                        <input type="submit" name="submit" value="Register" />
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
 </form>
-
 </body>
-</html> 
+</html>
