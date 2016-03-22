f0d2e7fa8c8bb74cd2ed22ea6aa75363b5d7af94
gith2 <hanuman55>
2016-03-22 08:47:41 +0000

    gg

--- coursework/bugs/bugs.php
+++ coursework/bugs/bugs.php
@@ -93,7 +93,7 @@ include("submit.php");
         </table>
         <section>
             <h1>Comments</h1>
-            <table>
+            <table width="500"  border="0" cellpadding="10" cellspacing="10">
             <tr>
                 <td colspan="2" align="centre" class="error"><?php echo $msg;?></td>
             </tr>
