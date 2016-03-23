3bfb50aa8398ebcacd5a3cc2e070185e270b106a
gith2 <hanuman55>
2016-03-23 21:43:09 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -10,7 +10,7 @@ if(isset($_POST["submit"]))
    $bugtitle= mysqli_real_escape_string($db, $bugtitle);
     $bugdesc = mysqli_real_escape_string($db, $bugdesc);
 
-    $query = mysqli_query($db, "INSERT INTO bugs (title, desc )VALUES ('bugtitle',  '$bugdesc')")or die(mysqli_error($db));
+    $query = mysqli_query($db, "INSERT INTO bugs (title, desc )VALUES ('$bugtitle',  '$bugdesc')")or die(mysqli_error($db));
     if($query)
     {
         $msg = "Thank You! you are now registered.";
