b4fabffa8d7f19817a2d67362fde216d01c0645f
gith2 <hanuman55>
2016-03-23 00:03:38 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -1,34 +1,56 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Bugs List</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<div id="content">
-    <?php
-    include("connection.php");
-$sql="SELECT *FROM bugs WHERE bugs.ID=".$_GET["id"];
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_assoc($result);
-    $bugTitle =$row["title"];
-    $bugID=$row["ID"];
-    $bugDesc=$row['desc'];
-    echo"<h2>".$bugTitle."</h2>";
-    echo"<p>".$bugDesc."</p>";
-    $sql="SELECT *FROM comments WHERE bugID =".$_GET["id"];
-    $result=mysqli_query($db,$sql);
-    while($row=mysqli_fetch_assoc($result))
-    {
-        $commentTitle=$row['title'];
-        $comment=$row['comment'];
-        echo'<h3>'.$title.'</h3>';
-        echo'<p>'.$comment.'</p>';
+<?php
+include("connection.php");
+$sql="SELECT * FROM bugs WHERE bugs.ID = ".$_GET["id"];
+$result=mysqli_query($db,$sql);
+while($row=mysqli_fetch_assoc($result))
+{
+    $bugtitle = $row['title'];
+    $bugID = $row['bugID'];
+    $bugdesc = $_POST["desc"];
+    $userId = $_POST["userID"];
+    $dateposted = $_POST["postDate"];
+    $datefixed = $_POST["fixDate"];
+    $fixedunfixed = $_POST["fixed"];
+}
 
+
+/*//$msg = "";
+if(isset($_POST["submit"]))
+{
+    $userid = $_POST["userid"];
+    $bugdesc = $_POST["bugdesc"];
+    $userId = $_POST["userId"];
+    $dateposted = $_POST["dateposted"];
+    $datefixed = $_POST["datefixed"];
+    $fixedunfixed = $_POST["fixedunfixed"];
+
+
+    $userid = mysqli_real_escape_string($db, $userid);
+    $bugdesc = mysqli_real_escape_string($db, $bugdesc);
+    $userId = mysqli_real_escape_string($db, $userId);
+    $dateposted = mysqli_real_escape_string($db, $dateposted);
+    $datefixed = mysqli_real_escape_string($db, $datefixed);
+    $fixedunfixed = mysqli_real_escape_string($db, $datefixed);
+
+
+
+    $sql="SELECT * FROM bugs WHERE bugs.ID=".$_GET["id"];
+    $result=mysqli_query($db,$sql);
+    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    if(mysqli_num_rows($result) == 1)
+    {
+        $msg = "Sorry...This email already exists...";
+    }
+    else
+    {
+        // echo $name." ".$email." ".$password;
+
+        $query = mysqli_query($db, "INSERT INTO users (userID, username, email,  password, phone)VALUES ('$userid', '$name', '$email',  '$password', '$phone')")or die(mysqli_error($db));
+        if($query)
+        {
+            $msg = "Thank You! you are now registered.";
+        }
 
     }
-    ?>
-    </div>
-</body>
-</html>
+}*/
+?>
