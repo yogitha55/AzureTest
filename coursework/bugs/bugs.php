401ac740398cb28d56259baa46123921c53e145e
gith2 <hanuman55>
2016-03-24 22:48:40 +0000

    gg

--- coursework/bugs/bugs.php
+++ coursework/bugs/bugs.php
@@ -87,7 +87,7 @@ include("check.php");
                     <div align="right"><label for="userId">User Id</label></div>
                 </td>
                 <td>
-                    <input name="userId" type="number" modifiable="false" class="input" size="8" value="<?php echo $login_user;?>">
+                    <input name="userId" type="number" readonly class="input" size="8" value="<?php echo $login_user;?>">
                 </td>
             </tr>
 
