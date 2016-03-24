38b336babd7e4ca3ee20d82c60297b8ff5cc95f8
gith2 <hanuman55>
2016-03-24 08:07:53 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -8,14 +8,14 @@ if(isset($_POST["submit"]))
     //$useriId = $_POST["userID"];
     //$postdate = strtotime("july 28 2014");
     $postdate = date ("Y");
-    $fixdate = date ("Y");
+
     $bugtitle= mysqli_real_escape_string($db, $bugtitle);
     $bugdesc = mysqli_real_escape_string($db, $bugdesc);
 
     //$query = mysqli_query($db, "INSERT INTO bugs (title, desc )VALUES ('$bugtitle', '$bugdesc')")or die(mysqli_error($db));
 
     $query = mysqli_query($db, "INSERT INTO bugs (bugID, title, desc, postDate, fixDate, fixed, userID)
-VALUES ('2', $bugtitle, '$bugdesc', '$postdate', '$fixdate' , NULL, '123')")or die(mysqli_error($db));
+VALUES ('1', $bugtitle, '$bugdesc', '$postdate', '$postdate' , 'NULL', '123')")or die(mysqli_error($db));
     ;
 
     if($query)
