51caca8aa06fd1880515c6761318363a756f5797
gith2 <hanuman55>
2016-03-24 22:18:08 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -6,25 +6,7 @@ if(isset($_POST["submit"]))
      $bugId = $_POST["bugId"];
      $bugtitle = $_POST["bugtitle"];
      $bugdesc = $_POST["bugdesc"];
-    //$postdate = strtotime("july 28 2014");
- //   $postdate = $_POST['Y-m-d H:i:s'];
-   // $fixdate = $_POST['Y-m-d H:i:s'];
-    //$fixed = $_POST["fixed"];
-      $userId = $_POST["userId"];
 
-
-    // echo $bugId." ".$bugtitle." ".$bugdesc." ".$postdate." ".$fixdate." ".$fixed." ".$userId;
-
-    // $postdate = strftime("%b %d, %Y", strtotime($row["postdate"]));
-    //$postdate = $row["postdate"];
-
-  /* $bugId = mysqli_real_escape_string($db, $bugId);
-  $bugtitle= mysqli_real_escape_string($db, $bugtitle);
-     $bugdesc = mysqli_real_escape_string($db, $bugdesc);
-   $postdate = mysqli_real_escape_string($db, $postdate);
-    $fixdate = mysqli_real_escape_string($db, $fixdate);
-    $fixed = mysqli_real_escape_string($db, $fixed);
-    $userId = mysqli_real_escape_string($db, $userId);*/
 
        if($bugtitle == null)
        {
@@ -32,14 +14,6 @@ if(isset($_POST["submit"]))
           $bugtitle = 'test123';
        }
 
-
-
-    //$query = mysqli_query($db, "INSERT INTO bugs (title, desc )VALUES ('$bugtitle', '$bugdesc')")or die(mysqli_error($db));
-   // $sql="SELECT *FROM users_old";
-    //$result=mysqli_query($db,$sql);
-    //$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    //$bugtitle = $row['title'];
-   // $bugID = $row['userID'];
 
 
 
