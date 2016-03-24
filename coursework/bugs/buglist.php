c845dee05d6adcf01a28b08b1fd4a51846a9e93d
gith2 <hanuman55>
2016-03-24 11:26:45 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -3,28 +3,29 @@
 $msg = "";
 if(isset($_POST["submit"]))
 {
+    $bugId = $_POST["bugId"];
     $bugtitle = $_POST["bugtitle"];
     $bugdesc = $_POST["bugdesc"];
-    $useriId = $_POST["userId"];
     //$postdate = strtotime("july 28 2014");
     $postdate = $_POST['Y-m-d H:i:s'];
     $fixdate = $_POST['Y-m-d H:i:s'];
     $fixed = $_POST["fixed"];
-    $bugId = $_POST["bugId"];
+    $useriId = $_POST["userId"];
+
 
 
 
     // $postdate = strftime("%b %d, %Y", strtotime($row["postdate"]));
     //$postdate = $row["postdate"];
 
+    $bugId = mysqli_real_escape_string($db, $bugId);
     $bugtitle= mysqli_real_escape_string($db, $bugtitle);
-    $bugdesc = mysqli_real_escape_string($db, $bugdesc);
-    $userId = mysqli_real_escape_string($db, $userId);
     $bugdesc = mysqli_real_escape_string($db, $bugdesc);
     $postdate = mysqli_real_escape_string($db, $postdate);
     $fixdate = mysqli_real_escape_string($db, $fixdate);
     $fixed = mysqli_real_escape_string($db, $fixed);
-    $bugId = mysqli_real_escape_string($db, $bugId);
+    $userId = mysqli_real_escape_string($db, $userId);
+
 
     //$query = mysqli_query($db, "INSERT INTO bugs (title, desc )VALUES ('$bugtitle', '$bugdesc')")or die(mysqli_error($db));
    /* $sql="SELECT *FROM bugs";
@@ -33,8 +34,9 @@ if(isset($_POST["submit"]))
     $bugtitle = $row['title'];
     $bugID = $row['ID'];*/
 
-    $query = mysqli_query($db, "INSERT INTO `bugs` (`bugID`, `title`, `desc`, `postDate`, `fixDate`, `fixed`, `userID`)
-VALUES ('bugId', '$bugtitle', '$bugdesc', '$postdate', '$fixdate', '$fixed', 'userId')")or die(mysqli_error($db));
+    $query = mysqli_query($db, "INSERT INTO bugs (bugID, title, desc, postDate, fixDate, fixed, userID)
+VALUES ('bugId', '$bugtitle', '$bugdesc', '$postdate', '$fixdate' , 'fixed', 'userId')")or die(mysqli_error($db));
+
 
 
     if($query)
