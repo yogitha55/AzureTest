b5e6f0026b6a19897357f2a4f41586cb41a683d9
gith2 <hanuman55>
2016-03-24 22:23:40 +0000

    gg

--- coursework/bugs/bugs.php
+++ coursework/bugs/bugs.php
@@ -90,19 +90,101 @@ include("buglist.php");
                     <input name="userId" type="number" class="input" size="8" >
                 </td>
             </tr>
+
+
+        </table>
+         <section>
+            <h1>Comments</h1>
+            <table>
+                <tr>
+                    <td colspan="2" align="centre" class="error"><?php echo $msg;?></td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right"><label for="name">Comment </label></div>
+                    </td>
+                    <td>
+                        <textarea name="comment" rows="5" columns="20" ></textarea>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right">
+                            <label for="usercommented">User Id</label>
+                        </div>
+                    </td>
+                    <td>
+                        <input name="userId" type="int" class="input" size="8" >
+
+                    </td>
+
+                </tr>
+                <td style="font-weight: bold">
+                    <div align="right">
+                        <label for="bugid">Bug Id</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="bugId" type="number" class="input" size="8" >
+
+                </td>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right">
+                            <label for="dateposted">Date Posted</label>
+                        </div>
+                    </td>
+                    <td>
+                        <input name="dateposted" type="datetime-local" class="input" size="11" >
+
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right">
+                            <label for="commentId">Comment Id</label>
+                        </div>
+                    </td>
+                    <td>
+                        <input name="commentId" type="number" class="input" size="8" >
+
+                    </td>
+                </tr>
+
+            </table>
+        </section>
+        <section>
+            <h1>Attachments</h1>
+            <table>
+
+                <table width="400"  border="0" cellpadding="10" cellspacing="10">
+                    <tr>
+                        <td colspan="2" align="centre" class="error"><?php echo $msg;?></td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold">
+                            <div align="right"><label for="name">File Upload</label></div>
+                        </td>
+                        <td>
+                            <input name="fileToUpload" type="file" id="fileToUpload"class="input">
+                            <input type="submit" value="Upload File" name="submit">
+                        </td>
+                    </tr>
                     <tr>
                         <td height="23"></td>
                         <td>
                             <div align="right">
+                                <input type="submit" name="submit" value="BugRegister" />
 
-                         <a href="comments.php">Comments</a>
+
 
 
                             </div>
                         </td>
                     </tr>
                 </table>
-
+        </section>
 
 
     </fieldset>
