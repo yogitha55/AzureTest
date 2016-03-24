f6e77fdd00d105085cafd1f78bc5c2c757c70264
gith2 <hanuman55>
2016-03-24 16:53:28 +0000

    gg

---
+++ coursework/bugs/attachments.php
@@ -0,0 +1,52 @@
+<?php
+
+include("connection.php");
+include("buglist.php");
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>BugsList</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<!--Attachments Information-->
+<body>
+<form method="post" action="">
+    <fieldset>
+            <h1>Attachments</h1>
+            <table>
+
+                <table width="400"  border="0" cellpadding="10" cellspacing="10">
+                    <tr>
+                        <td colspan="2" align="centre" class="error"><?php echo $msg;?></td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold">
+                            <div align="right"><label for="name">File Upload</label></div>
+                        </td>
+                        <td>
+                            <input name="fileToUpload" type="file" id="fileToUpload"class="input">
+                            <input type="submit" value="Upload File" name="submit">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td height="23"></td>
+                        <td>
+                            <div align="right">
+                                <input type="submit" name="submit" value="BugRegister" />
+
+
+
+
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+
+    </fieldset>
+</form>
+</body>
+</html>
---
+++ coursework/bugs/comments.php
@@ -0,0 +1,84 @@
+<?php
+
+include("connection.php");
+include("buglist.php");
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>BugsList</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<!--Comments Informataion-->
+<body>
+<form method="post" action="">
+    <fieldset>
+        <h1>Comments</h1>
+            <table>
+                <tr>
+                    <td colspan="2" align="centre" class="error"><?php echo $msg;?></td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right"><label for="name">Comment </label></div>
+                    </td>
+                    <td>
+                        <textarea name="comment" rows="5" columns="20" ></textarea>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right">
+                            <label for="usercommented">User Id</label>
+                        </div>
+                    </td>
+                    <td>
+                        <input name="userId" type="int" class="input" size="8" >
+
+                    </td>
+
+                </tr>
+                <td style="font-weight: bold">
+                    <div align="right">
+                        <label for="bugid">Bug Id</label>
+                    </div>
+                </td>
+                <td>
+                    <input name="bugId" type="number" class="input" size="8" >
+
+                </td>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right">
+                            <label for="dateposted">Date Posted</label>
+                        </div>
+                    </td>
+                    <td>
+                        <input name="dateposted" type="datetime-local" class="input" size="11" >
+
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-weight: bold">
+                        <div align="right">
+                            <label for="commentId">Comment Id</label>
+                        </div>
+                    </td>
+                    <td>
+                        <input name="commentId" type="number" class="input" size="8" >
+
+                    </td>
+                </tr>
+
+            </table>
+
+
+    </fieldset>
+</form>
+</body>
+</html>
+
