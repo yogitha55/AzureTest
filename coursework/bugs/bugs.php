d0c8780b407dad7f04975fcb6969924a6c8289f2
gith2 <hanuman55>
2016-03-24 22:27:14 +0000

    gg

--- coursework/bugs/bugs.php
+++ coursework/bugs/bugs.php
@@ -87,7 +87,7 @@ include("buglist.php");
                     <div align="right"><label for="userId">User Id</label></div>
                 </td>
                 <td>
-                    <input name="userId" type="number" class="input" size="8" >
+                    <input name="userId" type="number" class="input" size="8" value=$login_user>
                 </td>
             </tr>
 
