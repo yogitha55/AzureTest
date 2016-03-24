5f0cdd9e71bd8ab7f7c03559a2af38cca0977516
gith2 <hanuman55>
2016-03-24 10:25:32 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -3,8 +3,8 @@
 $msg = "";
 if(isset($_POST["submit"]))
 {
-    $value1 = $_POST['bugtitle'];
-    $value2 = $_POST["bugdesc"];
+    $bugtitle = $_POST["bugtitle"];
+    $bugdesc = $_POST["bugdesc"];
     //$useriId = $_POST["userID"];
     //$postdate = strtotime("july 28 2014");
     $postdate = date ("Y");
@@ -13,14 +13,14 @@ if(isset($_POST["submit"]))
     $bugdesc = mysqli_real_escape_string($db, $bugdesc);
 
     //$query = mysqli_query($db, "INSERT INTO bugs (title, desc )VALUES ('$bugtitle', '$bugdesc')")or die(mysqli_error($db));
-   // $sql="SELECT *FROM bugs";
-    //$result=mysqli_query($db,$sql);
-    //$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    //$bugtitle = $row['title'];
-    //$bugID = $row['ID'];
+   /* $sql="SELECT *FROM bugs";
+    $result=mysqli_query($db,$sql);
+    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $bugtitle = $row['title'];
+    $bugID = $row['ID'];*/
 
-    $query = mysqli_query($db, "INSERT INTO `bugs`(bugID, title, desc, postDate, fixDate, fixed, userID)
-VALUES ('$value', '$value2', '$value3', '$value4', '$value5', '$value6', '$value7',)")or die(mysqli_error($db));
+    $query = mysqli_query($db, "INSERT INTO `bugs` (bugID, title, desc, postDate, fixDate, fixed, userID)
+VALUES ('2', '$bugtitle', '$bugdesc', '$postdate', '$postdate', NULL, '123')")or die(mysqli_error($db));
 
 
     if($query)
