15db84991b8f630bfafaac3cddb561ccf1a0a1b3
gith2 <hanuman55>
2016-03-24 23:11:11 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -9,25 +9,17 @@ if(isset($_POST["submit"]))
       $userid = $_POST["userId"];
 
 
-       if($bugtitle == null)
-       {
-          echo $bugtitle;
-          $bugtitle = 'jdssdj';
-       }
-
-
-
 
     //$query = mysqli_query($db, "INSERT INTO users_old (`bugID`, `title`, `desc`,`userID`,`postDate`) VALUES ( default,'$bugtitle', '$bugdesc', default, NULL )")or die(mysqli_error($db));
 
-        $query = mysqli_query($db, "INSERT INTO bugs (`bugID`, `title`, `desc`, `userID`, `postDate`, `fixDate`, `fixed`) VALUES ( default,'$bugtitle', '$bugdesc', '$userId', NULL, NULL, NULL )")or die(mysqli_error($db));
+        $query = mysqli_query($db, "INSERT INTO bugs (`bugID`, `title`, `desc`, `userID`, `postDate`, `fixDate`, `fixed`) VALUES ( default,'$bugtitle', '$bugdesc', '$userid', NULL, NULL, NULL )")or die(mysqli_error($db));
 
 
-     $sql="SELECT budID FROM bugs WHERE title='$bugtitle'  and userID='$userId'";
+     $sql="SELECT budID FROM bugs WHERE title='$bugtitle'  and userID='$userid'";
      $result=mysqli_query($db,$sql);
      $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 
-     
+
 
 
     if($query)
