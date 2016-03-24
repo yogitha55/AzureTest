689ff3f244e3b1accea8d48e3220a6926d3d3b05
gith2 <hanuman55>
2016-03-24 22:39:22 +0000

    gg

--- coursework/bugs/bugs.php
+++ coursework/bugs/bugs.php
@@ -2,7 +2,7 @@
 
 include("connection.php");
 include("buglist.php");
-
+include("check.php");
 ?>
 <!doctype html>
 <html>
---
+++ coursework/bugs/check.php
@@ -0,0 +1,16 @@
+<?php
+include('connection.php');
+session_start();
+$user_check=$_SESSION['username'];
+
+$ses_sql = mysqli_query($db,"SELECT userID FROM users WHERE username='$user_check' ");
+
+$row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+
+$login_user=$row['userID'];
+
+if(!isset($user_check))
+{
+header("Location: register.php");
+}
+?>
