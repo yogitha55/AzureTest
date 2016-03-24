b3198ba4656ee80cdf10d0ecffd5c14ae8ad9f0e
gith2 <hanuman55>
2016-03-24 23:21:22 +0000

    gg

--- coursework/bugs/buglist.php
+++ coursework/bugs/buglist.php
@@ -16,11 +16,11 @@ if(isset($_POST["submit"]))
         $query = mysqli_query($db, "INSERT INTO bugs (`bugID`, `title`, `desc`, `userID`, `postDate`, `fixDate`, `fixed`) VALUES ( default,'$bugtitle', '$bugdesc', '$userid', NULL, NULL, NULL )")or die(mysqli_error($db));
 
 
-     $sql="SELECT budID FROM bugs WHERE title='$bugtitle'  and userID='$userid'";
+     $sql="SELECT bugID FROM bugs WHERE title='$bugtitle'  and userID='$userid'";
      $result=mysqli_query($db,$sql);
      $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 
-      $bug_id=$row['budID'];
+      $bug_id=$row['bugID'];
 
       $query = mysqli_query($db, "INSERT INTO comments (`commentID`, desc`, `userID`, `postDate`, `bugID`) VALUES ( default, '$comment', '$userid', NULL, '$bug_id' )")or die(mysqli_error($db));
 
