dfd85825a3283c5d07bde320e684bcb32d9a4291
gith2 <hanuman55>
2016-03-25 11:02:00 +0000

    gg

--- coursework/login/home.php
+++ coursework/login/home.php
@@ -14,9 +14,9 @@
 <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
 <br><br><br>
 <tr>
-	<td colspan="2">Bugs</td>
+	<!--<td colspan="2">Bugs</td>-->
 	<td> <a href="http://yogitha.azurewebsites.net/coursework/bugs/bugs.php">Bugs Home</a> </td>
-</tr><br>
+</tr>
 <tr>
 	<td>
 		<a href="logout.php" style="font-size:18px">Logout?</a>
