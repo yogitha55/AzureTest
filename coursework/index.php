70d07483e724a979a8e12d07568e1cb3df01e73e
gith2 <hanuman55>
2016-03-25 09:41:59 +0000

    gg

--- coursework/index.php
+++ coursework/index.php
@@ -18,7 +18,7 @@
 <!-- User Login Form Details-->
 <body>
 <div align="center">
-<h1 style="font-family:Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif; font-size:32px;"> User Login  </h1>
+<h1 style="font-family:Arial, Helvetica, sans-serif; font-size:30px;"> User Login  </h1>
 <div class="loginBox">
     <h3>Login Form</h3>
     <br><br>
@@ -27,7 +27,7 @@
         <input type="text" name="username" placeholder="username" /><br><br>
         <label>Password</label><br>
         <input type="password" name="password" placeholder="password" />  <br><br>
-        <input type="submit" name="submit" value="Login" /> 
+        <input type="submit" name="submit" value="Login" />
         <!-- Redirect to register page,,if user is not registred they can click on register button -->
         <p> Not Registred? Please Register Here</p>
         <a href="registration/register.php">Register</a>
