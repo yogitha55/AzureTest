8a26ebd49148fc5498df6503d47fb9a92d960515
gith2 <hanuman55>
2016-04-25 18:36:41 +0100

    gg

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -10,6 +10,8 @@ define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'be6310e7f09681');
 define('DB_PASSWORD', '4f8a3fc1');
 define('DB_DATABASE', 'yogitha_0917852');
-$db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+//$db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
