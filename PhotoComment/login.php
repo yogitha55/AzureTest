6ce76581a90037a61c7d90d2e2365f70eb81740a
gith2 <hanuman55>
2016-04-25 19:50:18 +0100

    gg

--- PhotoComment/login.php
+++ PhotoComment/login.php
@@ -1,62 +1,39 @@
 <?php
-	session_start();
-	include("connection.php"); //Establishing connection with our database
-	
-	$error = ""; //Variable for storing our errors.
-	if(isset($_POST["submit"]))
+session_start();
+include("connection.php"); //Establishing connection with our database
+
+$error = ""; //Variable for storing our errors.
+if(isset($_POST["submit"]))
+{
+	if(empty($_POST["username"]) || empty($_POST["password"]))
 	{
-		if(empty($_POST["username"]) || empty($_POST["password"]))
+		$error = "Both fields are required.";
+	}else
+	{
+		// Define $username and $password
+		$username=$_POST['username'];
+		$password=$_POST['password'];
+
+
+
+		//Check username and password from database
+		$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+		$result=mysqli_query($db,$sql);
+		$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+
+		//If username and password exist in our database then create a session.
+		//Otherwise echo error.
+
+		if(mysqli_num_rows($result) == 1)
 		{
-			$error = "Both fields are required.";
+			$_SESSION['username'] = $username; // Initializing Session
+			header("location: photos.php"); // Redirecting To Other Page
 		}else
 		{
-			// Define $username and $password
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-            /*
-			$username = stripslashes($username);
-			$password = stripslashes($password);
-			$username = mysqli_real_escape_string($db, $username);
-			$password = mysqli_real_escape_string($db, $password);
-            $password = md5($password); */
-
-			/* Prepared statement, stage 1: prepare */
-			if (!($stmt = $mysqli->prepare("INSERT INTO test(id) VALUES (?)")))
-			{
-				echo "Prepare failed: (" . $mysqli->erno . ") " . $mysqli->error;
-			}
-
-			/* Prepared statement, stage 2: bind and execute */
-			$id = 1;
-			if (!$stmt->bind_param("i", $id))
-			{
-				echo "Binding parameters failed: (" . $stmt->errno . ")" . $stmt->error;
-			}
-			/* Execute it */
-			if (!$stmt->execute())
-			{
-				echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-			}
-
-
-			//Check username and password from database
-			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql);
-			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
-			
-			//If username and password exist in our database then create a session.
-			//Otherwise echo error.
-			
-			if(mysqli_num_rows($result) == 1)
-			{
-				$_SESSION['username'] = $username; // Initializing Session
-				header("location: photos.php"); // Redirecting To Other Page
-			}else
-			{
-				$error = "Incorrect username or password.";
-			}
-
+			$error = "Incorrect username or password.";
 		}
+
 	}
+}
 
 ?>
