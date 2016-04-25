a8efa214987c1d1d8663dc4b9a8654abd52c6499
gith2 <hanuman55>
2016-04-25 22:33:29 +0100

    gg

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -10,8 +10,41 @@ define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'be6310e7f09681');
 define('DB_PASSWORD', '4f8a3fc1');
 define('DB_DATABASE', 'yogitha_0917852');
+
+//Create Connection
 $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 //$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $username, $email, $password);
+
+// set parameters and execute
+$username = "Hanu";
+$email = "john@example.com";
+$password = "hanu";
+$stmt->execute();
+
+$username = "Budugu";
+$email = "budugu@gmail.com";
+$password = "hello";
+$stmt->execute();
+
+$username = "Tata";
+$email = "tata@example.com";
+$password = "super";
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+$conn->close();
 ?>
+
+
+
