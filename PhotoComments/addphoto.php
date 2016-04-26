a68856f60f26d106bffacd3ae068fe6bb1996572
gith2 <hanuman55>
2016-04-26 21:17:04 +0100

    gg

--- PhotoComments/addphoto.php
+++ PhotoComments/addphoto.php
@@ -15,6 +15,11 @@ if(isset($_POST["submit"]))
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     $uploadOk = 1;
 
+    //checks mime type of the file being uploaded
+   $finfo = new finfo(FILEINFO_MIME_TYPE);
+   $fileContents = file_get_contents($_FILES['some_name']['tmp_name']);
+   $mimeType = $finfo->buffer($fileContents);  
+
     $sql="SELECT userID FROM users WHERE username='$name'";
     $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
@@ -40,6 +45,41 @@ if(isset($_POST["submit"]))
     else{
         $msg = "You need to login first";
     }
+    // Check if image file is a original or fake
+
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image -" . $check["mime"] . ".";
+            $uploadOk = 1;
+        }
+        else{
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+        //Check if file already exists
+
+        if(file_exists($target_file))
+        {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        //Check file size
+        if($_FILES["fileToUpload"]["Size"]>500000)
+        {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+
+        //Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif")
+        {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
 }
 
 ?>
