4b8f936474b7d62401fe3f24e34991c0b780925a
0917852 <alapati.rgu@outlook.com>
2016-04-26 14:49:14 +0100

    new message

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -7,9 +7,9 @@ define('DB_DATABASE', 'rgutest');
 //$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);*/
 
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'be6310e7f09681');
-define('DB_PASSWORD', '4f8a3fc1');
-define('DB_DATABASE', 'yogitha_0917852');
+define('DB_USERNAME', 'b115c751a37549');
+define('DB_PASSWORD', '10ccfeef');
+define('DB_DATABASE', 'yogitha1_0917852');
 
 //Create Connection
 $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
