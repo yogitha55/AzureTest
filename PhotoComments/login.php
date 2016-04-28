a4991abb8cf801564f08e824c8d8df7dea4e7a03
gith2 <hanuman55>
2016-04-28 20:13:17 +0100

    gg

--- PhotoComments/login.php
+++ PhotoComments/login.php
@@ -21,6 +21,13 @@ if(isset($_POST["submit"]))
 			$username=$_POST['username'];
 			$password=$_POST['password'];
 
+			//my sql injection
+			$username = stripslashes($username);
+			$password = stripslashes($password);
+			$usernmae = mysqli_real_escape_string($db, $username);
+			$password = mysqli_real_escape_string($db, $password);
+			$password = md5($password);
+
 			//Check username and password from database
 			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
 			$result=mysqli_query($db,$sql);
