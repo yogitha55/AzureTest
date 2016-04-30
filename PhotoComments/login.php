71e947adab576a80df7b1f03bbe95ab01dffef38
gith2 <hanuman55>
2016-04-30 23:30:12 +0100

    gg

--- PhotoComments/login.php
+++ PhotoComments/login.php
@@ -14,6 +14,15 @@ else{
 	$_SESSION['timeout'] = time();
 }*/
 
+if(isset($_SESSION['last_ip']) === false)
+{
+	$_SESSION['last_ip'] = $_SERVER['REMOTE_ADDR'];
+}
+if($_SESSION['last_ip'] !== $_SERVER['REMOTE_ADDR'])
+{
+	session_unset();
+	session_destroy();
+}
 
 
 if(isset($_POST["submit"]))
